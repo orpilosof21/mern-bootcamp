81a559ca1eea0e005555968104ceb17eb1de4dad
Or Eli Pilosof <44499215+orpilosof21@users.noreply.github.com>
2021-06-19 18:00:47 +0300

    Section 5, 38
    Starting setup, pages & routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Route, BrowserRouter as Router, Redirect, Switch} from 'react-router-dom';
 
-function App() {
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Lets go</h1>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/"/>
+      </Switch>
+    </Router>
   );
 }
 
