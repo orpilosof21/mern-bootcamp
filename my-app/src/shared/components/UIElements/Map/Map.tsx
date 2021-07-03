366e5c0a39d96b9fd1789fdf87b5bcd61fbb7631
Or Eli Pilosof <44499215+orpilosof21@users.noreply.github.com>
2021-07-03 16:05:10 +0300

    Section 5, 53 - not working

---
+++ my-app/src/shared/components/UIElements/Map/Map.tsx
@@ -0,0 +1,48 @@
+import React, { CSSProperties, useEffect, useRef } from "react";
+
+import { Loader } from "@googlemaps/js-api-loader"
+
+import './Map.css'
+
+
+const loader = new Loader({
+    apiKey: "AIzaSyC30YW39Ji0oNvPY4DbdfiEzdUS2-qrKGk",
+    version: "weekly",
+  });
+
+interface IMap{
+    name?: string,
+    style?: CSSProperties,
+    center: google.maps.LatLngLiteral,
+    zoom: number,
+}
+
+
+
+const Map = (props:IMap) => {
+    const mapRef = useRef<HTMLDivElement>(null);
+
+    const { center, zoom } = props;
+
+    function initMap(){
+        const map = new window.google.maps.Map(mapRef.current as HTMLElement, {
+            center: center,
+            zoom: zoom
+          });
+        
+        const marker = new window.google.maps.Marker({ position: center, map: map });
+        console.log(map);
+        console.log(marker);
+    }
+
+    useEffect(() => {
+        loader.loadCallback(initMap);
+      }, [center, zoom]);  
+
+    return(
+        <div ref={mapRef} className={`map ${props.name}`} style={props.style}>
+        </div>
+    );
+}
+
+export default Map;
---
+++ my-app/src/shared/components/UIElements/Modal/Modal.tsx
@@ -0,0 +1,60 @@
+import React, { CSSProperties } from "react";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "../Backdrop/Backdrop";
+import './Modal.css';
+
+
+export interface IModal{
+    className?:string,
+    style?:CSSProperties,
+    headerClass?:string,
+    header:string,
+    onSubmit?: () => void,
+    contentClass?: string,
+    footerClass?: string,
+    footer:React.ReactNode,
+    children?: React.ReactNode,
+    show: boolean,
+    onCancel: () => void,
+    innerRef?: any
+}
+
+const ModalOverlay = (props : IModal) => {
+    const content = (
+        <div ref={props.innerRef} className={`modal ${props.className}`} style={props.style}>
+          <header className={`modal__header ${props.headerClass}`}>
+            <h2>{props.header}</h2>
+          </header>
+          <form
+            onSubmit={
+              props.onSubmit ? props.onSubmit : event => event.preventDefault()
+            }
+          >
+            <div className={`modal__content ${props.contentClass}`}>
+              {props.children}
+            </div>
+            <footer className={`modal__footer ${props.footerClass}`}>
+              {props.footer}
+            </footer>
+          </form>
+        </div>
+      );
+
+      return content;
+};
+
+const Modal = (props: IModal) => {
+    const modalNodeRef = React.useRef(null);
+
+    return(
+        <React.Fragment>
+            {props.show && <Backdrop onClick={props.onCancel}/>}
+            <CSSTransition nodeRef={modalNodeRef} in={props.show} mountOnEnter unmountOnExit timeout={200} classNames="modal">
+                <ModalOverlay innerRef={modalNodeRef} {...props}/>
+            </CSSTransition>
+        </React.Fragment>
+    )
+};
+
+export default Modal;
