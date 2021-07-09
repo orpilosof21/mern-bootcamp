87b79ea58505bb60a5ddfac54f4046335a0e524b
Or Eli Pilosof <44499215+orpilosof21@users.noreply.github.com>
2021-07-09 16:45:46 +0300

    Section 5, 58

--- my-app/src/places/pages/NewPlace/NewPlace.tsx
+++ my-app/src/places/pages/NewPlace/NewPlace.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Input from "../../../shared/components/FormElements/Input/Input";
+import { VALIDATOR_REQUIRE } from "../../../shared/Utils/validators";
 
 import './NewPlace.css';
 
 const NewPlace = () => {
     return (
         <form className="place-form">
-            <Input element="input" type="text" label="Title"/>
+            <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a new place."/>
         </form>
     )
 };
--- my-app/src/shared/components/FormElements/Input/Input.tsx
+++ my-app/src/shared/components/FormElements/Input/Input.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { type } from "os";
+import React, {useReducer} from "react";
+import { validate } from "../../../Utils/validators";
 
 import './Input.css';
 
@@ -8,21 +10,70 @@ interface IInput {
     type?:string,
     placeholder?:string
     rows?:number,
-    label:string
+    label:string,
+    errorText?: string,
+    validators?: any,
 
 }
 
+type ActionType = 'CHANGE' | 'TOUCH';
+
+interface ActionState {
+  value: string,
+  isValid: boolean,
+  isTouched: boolean
+}
+
+interface Action {
+  type: ActionType,
+  val?: string,
+  validators?: any,
+}
+
+
+
+const defaultErrorText = "ERROR";
+
+const inputReader = (state:ActionState, action:Action) : ActionState => {
+  switch(action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val || '',
+        isValid: validate(action.val || '', action.validators)
+      };
+      case 'TOUCH':
+        return {
+          ...state,
+          isTouched: true,
+        }
+      default:
+        return state;
+  }
+}
+
 const Input = (props:IInput) => {
+  // {value: '', isValid: false}
+  const [inputState, dispatch] = useReducer(inputReader, {value: '', isValid: false, isTouched: false});
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    dispatch({type: 'CHANGE', val: event.currentTarget.value as string, validators: props.validators});
+  }
+
+  const touchHandler = () => {
+    dispatch({type: 'TOUCH'})
+  }
     
     const element = props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} onBlur={touchHandler} value={inputState.value}/>
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} onBlur={touchHandler} value={inputState.value}/>
     );
 
-    return <div className={`form-control`}>
+    return <div className={`form-control ${!inputState.isValid && inputState.isTouched && 'form-control--invalid'}`}>
         <label htmlFor={props.id}>{props.label}</label>
         {element}
+        {!inputState.isValid && inputState.isTouched && <p>{props.errorText || defaultErrorText}</p>}
     </div>
 };
 
